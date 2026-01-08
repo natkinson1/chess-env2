8b1dc2f189c7a2cf3f5bcaa85ac14f8d958d1b2d
Nikhil Atkinson <nikhilatkinson1@gmail.com>
2026-01-08 11:32:53 +1100

    Configuring for a pypi package release and updating the readme and adding license

--- src/main.cpp
+++ src/main.cpp
@@ -8,12 +8,12 @@
 
 namespace py = pybind11;
 
-PYBIND11_MODULE(chess_env, m, py::mod_gil_not_used()) {
+PYBIND11_MODULE(chess_env_rl, m, py::mod_gil_not_used()) {
     py::class_<Board>(m, "ChessEnv")
         .def(py::init<>())
         .def("reset", &Board::reset, py::return_value_policy::reference_internal)
         .def("get_actions", &Board::get_legal_moves)
-        .def("print_board", &Board::print_board)
+        .def("render", &Board::print_board)
         .def("parse_fen", &Board::parse_fen)
         .def("step", &Board::step);
 }
